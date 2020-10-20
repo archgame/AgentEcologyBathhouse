366129fc89f8436b6e35edbf190b10fd2e19b0ca
Niveditha133 <69324426+Niveditha133@users.noreply.github.com>
2020-10-20 13:13:45 -0700

    D:\01_MASTERS\01_UCLA\Course Details\01_Fall Term\02_Tech Seminar\AgentEcologyBathhouse\Assets\Alpha\CameraController.cs  Camerascript

---
+++ Assets/Alpha/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    //public float panBoarderThickness = 10f;
+    public Vector3 panLimit;
+    //public Camera[] _cameras;
+
+  
+    private void Update()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            position.y += panSpeed * Time.deltaTime;
+        }
+        
+        if (Input.GetKey("s"))
+        {
+            position.y -= panSpeed * Time.deltaTime;
+        }
+        
+        if (Input.GetKey("d"))
+        {
+            position.x += panSpeed * Time.deltaTime;
+        }
+        
+        if (Input.GetKey("a"))
+        {
+            position.x -= panSpeed * Time.deltaTime;
+        }
+
+        position.x = Mathf.Clamp(position.x, -panLimit.x, panLimit.x);
+        position.y = Mathf.Clamp(position.y, -panLimit.y, panLimit.y);
+        position.z = Mathf.Clamp(position.z, -panLimit.z, panLimit.z);
+
+        //if (Input.GetKey("1"))
+        //{
+            //_cameras.shi(guest, Path[1].transform.position);
+            
+            //position.x -= panSpeed * Time.deltaTime;
+        //}
+
+        transform.position = position;
+    }
+}
