f2b3efc4bcd56f5ab82c0efa0714b5202fdbc24e
Kaiwen Wu <pattywkw@g.ucla.edu>
2020-11-10 21:31:42 -0800

    metrics1
    doubleheight change color

---
+++ Assets/Hotpot/scripts/Doubleheight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doubleheight : MonoBehaviour
+{
+
+    public Material Alt;
+    public float guestnumber = 0;
+    // public Action Status;
+    // public enum Action { BATHING, WALKING, FOLLOWING, RIDING, RANDOM }
+    
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!other.GetComponent<Guest>()) return;
+        Debug.Log("Guest in double height");
+
+
+        Guest guest = other.GetComponent<Guest>();
+        if (guest.Status == Guest.Action.BATHING) { other.GetComponent<Guest>().GuestWalkDestination(); }
+        guestnumber++;
+        Debug.Log(guestnumber);
+        MeshRenderer mr = other.GetComponent<MeshRenderer>();
+        mr.material = Alt;
+        guest.SetText("VIP BATHING!");
+        Debug.Log("BATHING");
+        guest.SetSlider(guestnumber);
+
+    }
+
+    
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (!other.GetComponent<Guest>()) return;
+
+    }
+}
