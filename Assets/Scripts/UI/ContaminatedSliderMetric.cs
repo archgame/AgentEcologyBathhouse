b3b0f120d936676f42c5a134067941dd5b020721
JourdonM <72241707+JourdonM@users.noreply.github.com>
2020-11-11 19:35:19 -0800

    Metrics Update
    Edited Slide Bar for all three metrics.
    Added Safe Exits counter (Graphic)

---
+++ Assets/Scripts/UI/ContaminatedSliderMetric.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+
+public class ContaminatedSliderMetric : MonoBehaviour
+{
+    public static Canvas Instance { get; private set; }
+    public Slider contaminatedSlider;
+    public Dictionary<Guest, List<Guest>> guestEncounters = new Dictionary<Guest, List<Guest>>();
+    private float contaminatedNumber = 0.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contaminatedSlider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contaminatedSlider.value = contaminatedNumber;
+    }
+
+}
---
+++ Assets/Scripts/UI/SickSliderMetric.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+
+public class SickSliderMetric : MonoBehaviour
+{
+    public static Canvas Instance { get; private set; }
+    public Slider sickSlider;
+    public Dictionary<Guest, List<Guest>> guestEncounters = new Dictionary<Guest, List<Guest>>();
+    private float sickNumber = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sickSlider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sickSlider.value = sickNumber;
+    }
+
+}
