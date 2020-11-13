dc1144e367191c2c822f787309c20c7c54563343
Niveditha133 <69324426+Niveditha133@users.noreply.github.com>
2020-11-12 20:30:55 -0800

    Fix

--- Assets/Alpha/Scripts/AgentColorChange.cs
+++ Assets/Alpha/Scripts/AgentColorChange.cs
@@ -3,42 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AgentColorChange : MonoBehaviour
-{
-           
+{           
     public Material Main;
     public Material Alt;
     public Material Alt1;
     public Material Alt2;
+    public Material Alt3;
+    public Material Alt4;
     public float Score = 0;
     
-
     public void OnTriggerEnter(Collider other)
     {
         if (!other.GetComponent<Guest>()) return;
         Debug.Log("Guest Encounter");
         Score += 1;
         MeshRenderer mr = other.GetComponent<MeshRenderer>();
-        if (Score < 2)
+        if (Score == 3)
         {
             mr.material = Alt;
         }
 
-        else if (Score < 50)
+        else if (Score == 6)
         {
             mr.material = Alt1;
         }
 
-        else if (Score < 75)
+        else if (Score == 10)
         {
             mr.material = Alt2;
         }
 
-        /*Guest guest = other.GetComponent<Guest>();
-        if (guest.Status == Guest.Action.RANDOM) { other.GetComponent<Guest>().GuestWalkDestination(); }
-        guest.SetText("Inside Atrium");
-        guest.SetSlider(1);*/
+        else if (Score == 15)
+        {
+            mr.material = Alt3;
+        }
+
+        else if (Score == 20)
+        {
+            mr.material = Alt4;
+        }        
     }
-
-
-
 }
